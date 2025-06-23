34c749e0555138cdf764744fce26744887b97449
cogeds <muthuvijayan.mutharasu@cognizant.com>
2025-06-23 19:34:23 +0530

    Update footer.js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -2,6 +2,7 @@ import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
 
 const decorateLinkItems = (footer) => {
+ const footer = document.createElement('div');
   // add wrapper cs class to all ul elements
   footer.querySelectorAll('ul').forEach((ul) => {
   //  createTag('li', { class: 'nav-items-wrapper' });
