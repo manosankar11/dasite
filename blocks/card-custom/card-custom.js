30c397ede0a7fce404421db35c805d1fb8339cbd
cogeds <muthuvijayan.mutharasu@cognizant.com>
2025-07-03 11:57:59 +0530

    Update card-custom.js

--- blocks/card-custom/card-custom.js
+++ blocks/card-custom/card-custom.js
@@ -1,22 +1,21 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 export default function decorate(block) {
-  const ul = document.createElement('ul');
-  console.log("ul created");
+   div.className = 'card-wrap';
   [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    console.log("li created");
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
+    //const li = document.createElement('li');
+    //console.log("li created");
+    while (row.firstElementChild) div.append(row.firstElementChild);
+    [...div.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'card-custom-image';
       else div.className = 'cards-custom-body';
     });
-    ul.append(li);
+    //ul.append(li);
   });
 
   // replace images with optimized versions
-  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+ // ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
 
-  block.replaceChildren(ul);
+  //block.replaceChildren(ul);
 
   
 }
