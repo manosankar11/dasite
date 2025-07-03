5bcb235fd16870fe5069411a96312ea0f19bd6bf
cogeds <muthuvijayan.mutharasu@cognizant.com>
2025-07-03 11:08:23 +0530

    Update card-custom.js

--- blocks/card-custom/card-custom.js
+++ blocks/card-custom/card-custom.js
@@ -1,12 +1,11 @@
 export default function decorate(block) {
-
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'card-custom-image';
+      else div.className = 'cards-custom-body';
     });
     ul.append(li);
   });
