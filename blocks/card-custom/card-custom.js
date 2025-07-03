54ae9063e458cf382d6844ec66203bc08e29cd78
cogeds <muthuvijayan.mutharasu@cognizant.com>
2025-07-03 11:06:32 +0530

    Update card-custom.js

--- blocks/card-custom/card-custom.js
+++ blocks/card-custom/card-custom.js
@@ -1,7 +1,20 @@
 export default function decorate(block) {
-[...block.children].forEach(row) => {
-  [...row.children].forEach(col) => {
-  console.log(col);
+
+  const ul = document.createElement('ul');
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    while (row.firstElementChild) li.append(row.firstElementChild);
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
+    });
+    ul.append(li);
   });
-});
+
+  // replace images with optimized versions
+  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+
+  block.replaceChildren(ul);
+
+  
 }
