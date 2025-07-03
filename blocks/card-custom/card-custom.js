918f4fe74860a3f13a452017e703b14e7bece3b7
cogeds <muthuvijayan.mutharasu@cognizant.com>
2025-07-03 10:31:38 +0530

    Update card-custom.js

--- blocks/card-custom/card-custom.js
+++ blocks/card-custom/card-custom.js
@@ -1,8 +1,8 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  const child=document.querySelectorAll('div > *');
-  child.forEach(child => {
+  const children=document.querySelectorAll('div > *');
+  children.forEach(child => {
 child.classList.add('test');
 }
 }
