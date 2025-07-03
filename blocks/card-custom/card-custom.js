da3bf84d34258ae634d196d196d8579ed736405d
cogeds <muthuvijayan.mutharasu@cognizant.com>
2025-07-03 12:08:54 +0530

    Update card-custom.js

--- blocks/card-custom/card-custom.js
+++ blocks/card-custom/card-custom.js
@@ -1,21 +1,12 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
+
 export default function decorate(block) {
    div.className = 'card-wrap';
   [...block.children].forEach((row) => {
-    //const li = document.createElement('li');
-    //console.log("li created");
-    while (row.firstElementChild) div.append(row.firstElementChild);
-    [...div.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'card-custom-image';
-      else div.className = 'cards-custom-body';
+     const divs=document.querySelectorAll('div);
+      divs.forEach(function(div)){
+         div.classList.add('test');
+      }
     });
-    //ul.append(li);
-  });
-
-  // replace images with optimized versions
- // ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-
-  //block.replaceChildren(ul);
 
   
 }
